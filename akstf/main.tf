bff4a8dcff1b727ab2cf0d8ea37eab8ac50fb511
sangeetakuntoji <sangeeta.kuntoji@gmail.com>
2023-08-31 01:02:29 +0200

    updated

---
+++ akstf/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.69.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "dev-rg"
+    storage_account_name = "devxyzstorageac01"
+    container_name       = "devconatiner"
+    key                  = "devaks.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "learnk8sResourceGroup"
+  location = "northeurope"
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = "learnk8scluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "learnk8scluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = "1"
+    vm_size    = "standard_d2_v2"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
